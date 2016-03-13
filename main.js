972e4bdfe1a3b0b46bd5f9973eeb1a9cb34db7f2
Jeremy Lubin <lubin.jeremy@gmail.com>
2016-03-12 20:59:37 -0800

    add url parsing for charity and reason

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import campaign from './reducers'
 import Campaign from './containers/Campaign'
+import parseUri from './vendor/parse-uri'
+
+const charity = decodeURI( parseUri( window.location ).queryKey['charity'] )
+const reason = decodeURI( parseUri( window.location ).queryKey['reason'] )
 
 let store = createStore(campaign, {
 	contribution: {
-		charityName: 'The Herren Project',
+		charityName: charity === "undefined" ? 'United States Association for UNHCR' : charity,
 		// charityName: 'Global Citizen Year',
-		reason: 'individuals suffering from addiction',
+		reason: reason === "undefined" ? 'Syrian refugees' : reason,
 		amount: 0
 	},
 	pages: [
